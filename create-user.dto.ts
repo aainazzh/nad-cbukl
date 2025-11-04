27997c791e3a9adf32a4af0a7ecccf4f4aedd9ce
Nadhifa <nadhifatulazmin29@gmail.com>
2025-11-04 15:12:20 +0700

    FOLDER DTO DI DALAM FOLDER USERS

---
+++ create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  role?: 'STUDENT' | 'STAFF' | 'ADMIN';
+}
